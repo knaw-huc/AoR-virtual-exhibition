0c7fb925df5bc23369be508980e05dd4398197f8
doppen <basdoppen@gmail.com>
2020-06-23 11:51:21 +0200

    images

--- img.js
+++ img.js
@@ -3,8 +3,9 @@ const fs = require('fs');
 const sharp = require('sharp');
 // https://sharp.pixelplumbing.com/api-resize
 
-const inputFolder  = './src/orig_artworks/';
-const outputFolder = './src/images/artworks/';
+// images
+const inputFolder  = './orig_img/';
+const outputFolder = './src/images/content/';
 
   fs.readdir(inputFolder, function (err, files) {
       //handling error
@@ -13,13 +14,32 @@ const outputFolder = './src/images/artworks/';
       }
 
       files.forEach(function (file) {
-
+        console.log(file);
         sharp(inputFolder+file)
-          .resize(400,500,{
-            fit: 'cover'
-          })
-          .toFile(outputFolder+'thumb_'+file, function(err) {
+          .resize({ width: 500 })
+          .toFile(outputFolder+file, function(err) {
           });
 
       });
   });
+
+
+  // content
+  const inputFolder2  = './orig_img/manuscripts_highres/';
+  const outputFolder2 = './src/images/manuscripts/';
+
+    fs.readdir(inputFolder2, function (err, files) {
+        //handling error
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+        }
+
+        files.forEach(function (file) {
+          console.log(file);
+          sharp(inputFolder2+file)
+            .resize({ width: 1200 })
+            .toFile(outputFolder2+file, function(err) {
+            });
+
+        });
+    });
