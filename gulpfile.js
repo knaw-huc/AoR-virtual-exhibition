5f9b68e1965334f1f3f80ed5ff600ca25cfcbaea
Bas Doppen <bas.doppen@di.huc.knaw.nl>
2020-06-24 16:09:05 +0200

    br

--- gulpfile.js
+++ gulpfile.js
@@ -513,7 +513,7 @@ function handletextPre(content) {
     content = content.replace(/<h2>±colspan±<\/h2>/g, "<p>±colspan±</p>");
     content = content.replace(/<p>±colspan±<\/p>/g, "<p>±col1span±</p>");
     content = content.replace(/<p>±col3±<br \/>/g, "<p>±col3±</p><p>");
-    content = content.replace(/<p>±row±<br \/>±col1± <\/p>/g, "<p>±row±</p><p>±col1±</p>");
+    content = content.replace(/<p>±row±<br \/>/g, "<p>±row±</p><p>");
 
 
     content = content.replace(/±timeline±/g, '<div class="">{{> timeline}}</div>');
