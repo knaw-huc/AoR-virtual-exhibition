f58f1cbcd2b8d5f3197e4244c125cef45889236d
Bas Doppen <basdoppen@gmail.com>
2020-11-13 09:39:30 +0100

    content update

--- gulpfile.js
+++ gulpfile.js
@@ -543,7 +543,7 @@ function handletextPre(content) {
     content = content.replace(/<p>±col1±<br \/>/g, "<p>±col1±</p><p>");
     content = content.replace(/]]]<br \/><\/p>/g, "]]]</p>");
     content = content.replace(/<p><br \/>±row±<br \/>±col1±<\/p>/g, "<p>±row±</p><p>±col1±</p>");
-
+    content = content.replace(/±<br \/>\[\[\[/g, "±</p><p>[[[");
 
 
 
